a962c541eb479b5335b95ab5b5eae04dc0fb78b5
dev-mahesh-peerxp <mahesh.kn@peerxp.com>
2024-10-06 22:50:23 +0530

    updated header

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,64 +1,57 @@
-// import React, { useState } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../Assets/png/logo_white.avif'
-import logo1 from '../Assets/png/logo_invert.jpeg'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+import MobileNavBar from './MobileNavBar'
 
 
 
 
-const Navbar = (props)=> {
-    const navMain={
-        backgroundColor:"black",
-        color:"white",
-        display:"flex",
-        alignItems:"center"
+const Navbar = (props) => {
+  const [openMenu, setOpenMenu] = useState(false)
 
-        }
-        // const [value, setValue]=useState();
-
-        //  const onChange =(e)=>{
-        //     setValue(e.target.value)
-        //  }
-        //  const onSearch =(searchTerm)=>{
-        //     //API call
-        //     console.log('search', searchTerm)
-        //  }
-   
   return (
     <>
-    <div className='header' style={navMain}>
-        <div className='links'>
-            <img className="logo" alt='' src={Logo}/>
-            <Link className='logo1'to={''}>
-                <img  alt='' src={logo1}/>
-            </Link>
+      <div className='header bg-black flex items-center justify-between lg:px-16 text-white'  >
+        <div className='links flex gap-4'>
+          <Link className='' to={''}>
+            <img className="logo" alt='' src={Logo} />
+          </Link>
+
         </div>
-        <div className='list'>
-        <ul>
-            <li><Link className='link' to={"games"}>Games</Link></li>
-            {/* <li><Link className='link' to={"news"}>News</Link></li> */}
-            <li><Link className='link' to={"aboutus"}>about us</Link></li>
-            <li><Link className='link' to={"support"}>support</Link></li>
-            
-                </ul>
+        <div onClick={() => setOpenMenu(!openMenu)} className='navTrigger flex gap-4 lg:hidden text-gray-100 cursor-pointer mx-4'>
+          {openMenu ?
+            <XMarkIcon className='size-6 text-white' /> :
+            <Bars3Icon className='size-6 text-white' />
+          }
         </div>
-       <div className="info">
-           <div className="searchBox">
-               <input placeholder='SEARCH'  className='inputbox' type='text'/>
-               <button  id="btn">
-                   <i class="fa-solid fa-magnifying-glass"></i>
-               </button>
-                   
-           </div>
-           <div className='signIn'>
-                 <Link to={"signin"}><button className='btn'>Sign in</button></Link>
-           </div>
-       </div>
-       
-       
-   </div>
-       
-       </>
+        {
+          openMenu &&
+          <MobileNavBar />
+        }
+
+        <div className=' items-center w-auto gap-28 hidden lg:flex '>
+          <div className='list '>
+            <ul>
+              <li><Link className='link' to={"games"}>Games</Link></li>
+              <li><Link className='link' to={"aboutus"}>about us</Link></li>
+              <li><Link className='link' to={"support"}>support</Link></li>
+            </ul>
+          </div>
+          <div className="info ">
+            <div className="searchBox flex">
+              <input placeholder='SEARCH' className='inputbox text-sm' type='text' />
+              <button id="btn">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </button>
+            </div>
+            <div className='signIn'>
+              <Link to={"signin"}><button className='btn  w-20'>Sign in</button></Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
