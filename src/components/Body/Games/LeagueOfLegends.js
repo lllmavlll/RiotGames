155531f7a25ff4a38ce1d7b43f53b7a92127ece5
dev-mahesh-peerxp <mahesh.kn@peerxp.com>
2024-10-09 02:29:45 +0530

    games section responsive

--- src/components/Body/Games/LeagueOfLegends.js
+++ src/components/Body/Games/LeagueOfLegends.js
@@ -1,39 +1,24 @@
 import React from 'react'
-import valGameplay  from './imges/LOL_gameplay.webm'
+import valGameplay from './imges/LOL_gameplay.webm'
+import GamePlayCTA from '../../UI/GamePlayCTA'
 
 
 const LeagueOfLegends = () => {
   return (
     <div className='valMainDiv'>
-        <div className='lolBan'>
-            <div className='valBanInputs'>
-              {/* <div className='valBanBtn'>
-                <button>play for free</button>
-                <button>watch trailer</button>
-              </div> */}
-            </div>
-        </div>
-        <div className='valoBan2'>
-          <div className='valoBan2Div1'>
-            <div className='gplay'>
-              <h1>Game play</h1>
-              <p>League of Legends, commonly referred to as League, is a 2009 multiplayer online battle arena video game developed and published by Riot Games. Inspired by Defense of the Ancients, a custom map for Warcraft III, Riot's founders sought to develop a stand-alone game in the same genre.<br/>
-              The primary reason for LoL's popularity is its accessibility. League of Legends is one of the first video games to introduce a free-to-play model to success. They released the game without any price tag. Instead, the company monetized through cosmetics that provided no in-game benefits aside from aesthetics.</p>
-            </div>
-            <video autoPlay loop muted playsInline className='gplay1'>
-                    <source src={valGameplay}/>
-                </video>
-          </div>
-          <div className='valoBan2Div2'>
-            <p>Avialable on</p> 
-            <div className='gameIbg'>
-              <i class="fa-brands fa-windows"></i>
-              <i class="fa-brands fa-apple"></i>
-            </div>
+      <div className='lolBan'>
+        <div className='valBanInputs'>
 
-          </div>
         </div>
-      
+      </div>
+      <GamePlayCTA
+        videoSrc={valGameplay}
+        description={`League of Legends, commonly referred to as League, is a 2009 multiplayer online battle arena video game developed and published by Riot Games. Inspired by Defense of the Ancients, a custom map for Warcraft III, Riot's founders sought to develop a stand-alone game in the same genre.
+              The primary reason for LoL's popularity is its accessibility. League of Legends is one of the first video games to introduce a free-to-play model to success. They released the game without any price tag. Instead, the company monetized through cosmetics that provided no in-game benefits aside from aesthetics.`}
+        iconsClass={
+          ['fa-brands fa-windows', 'fa-brands fa-apple']
+        }
+      />
     </div>
   )
 }
--- src/components/Body/Games/Tet.js
+++ src/components/Body/Games/Tet.js
@@ -1,40 +1,30 @@
 import React from 'react'
-import tetgplay  from './imges/TET_gplay.webm'
+import tetgplay from './imges/TET_gplay.webm'
+import GamePlayCTA from '../../UI/GamePlayCTA'
 
 
 const Tet = () => {
   return (
     <div className='valMainDiv'>
-        <div className='tetBan '>
-            <div className='valBanInputs'>
-              <div className='valBanBtn'>
-                <button>play for free</button>
-                <button>watch trailer</button>
-              </div>
-            </div>
-        </div>
-        <div className='valoBan2'>
-          <div className='valoBan2Div1'>
-            <div className='gplay'>
-              <h1>Game play</h1>
-              <p>Teamfight Tactics is an auto battler game developed and published by Riot Games. The game is a spinoff of League of Legends and is based on Dota Auto Chess, where players compete online against seven other opponents by building a team to be the last one standing.<br/>
-              According to President of Esports John Needham, TFT is “the number one strategy game in the world right now,” beating out many other popular strategy game titles like Starcraft and Hearthstone.
-              </p>
-            </div>
-            <video autoPlay loop muted playsInline className='gplay1'>
-                    <source src={tetgplay}/>
-                </video>
+      <div className='tetBan '>
+        <div className='valBanInputs'>
+          <div className='valBanBtn'>
+         
           </div>
-          <div className='valoBan2Div2'>
-            <p>Avialable on</p> 
-            <div className='gameIbg'>
-            <i class="fa-brands fa-windows"></i>
-            <i class="fa-brands fa-apple"></i>
-            <i class="fa-solid  fa-mobile"></i>
-            </div>
+        </div>
+      </div>
 
-          </div>
-        </div>
+      <GamePlayCTA
+        description={`Teamfight Tactics is an auto battler game developed and published by Riot Games. The game is a spinoff of League of Legends and is based on Dota Auto Chess, where players compete online against seven other opponents by building a team to be the last one standing.
+              According to President of Esports John Needham, TFT is “the number one strategy game in the world right now,” beating out many other popular strategy game titles like Starcraft and Hearthstone.
+              `}
+        videoSrc={tetgplay}
+        iconsClass={[
+          "fa-brands fa-windows",
+          "fa-brands fa-apple",
+          "fa-solid  fa-mobile"
+        ]}
+      />
     </div>
   )
 }
